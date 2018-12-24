768d292d962e744fa76617964af7c498b37696c4
weslyvinicius <weslyvinicius@hotmail.com>
2018-12-24 17:07:59 -0200

    arquivo de configuração d Swagger

---
+++ src/main/java/com/invillia/acme/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.invillia.acme.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Invilla Acme", "Orders API for Acme MVP.", "1.0", "Terms of service",
+				new Contact("Wesly Vinicius", "", "weslyvinicius@hotmail.com"), "License of API", "API license URL",
+				Collections.emptyList());
+	}
+
+}
